7d12b76a3d0e8ddfb8c3815c9e42354c3cb836ae
muneo1008 <ansj011008@gmail.com>
2024-08-11 03:02:56 +0900

    react

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Home from './pages/Home';
 import About from './pages/about';
 import Counter from './pages/Counter';
 import Input from './pages/input';
+import Input2 from './pages/input2';
+import List from './pages/List'
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/Counter">Counter</Link> | <Link to="/Input">Input</Link>
+        <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link> | 
+        <Link to="/Counter">Counter</Link> | 
+        <Link to="/Input">Input</Link> | 
+        <Link to="/Input2">Input2</Link> |
+        <Link to="/List">List</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/Counter' element={<Counter />}/>
         <Route path='/Input' element={<Input />}/>
+        <Route path='/Input2' element={<Input2 />}/>
+        <Route path='/List' element={<List />}/>
       </Routes>
 
     </div>
