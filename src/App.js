34cf9b79807e3d72cdf3982f4c0e0c5b9ad6fdfb
muneo1008 <ansj011008@gmail.com>
2024-08-19 09:47:41 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Routes, Route,Link} from 'react-router-dom';
-import { Button, Alert, Container, NavDropdown,Card,Accordion,Badge,Tab,ButtonGroup } from 'react-bootstrap';
+// import {Routes, Route,Link} from 'react-router-dom';
+// import { Button, Alert, Container, NavDropdown,Card,Accordion,Badge,Tab,ButtonGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from 'react';
 
 // import Home from './pages/Home';
 // import About from './pages/about';
@@ -12,24 +12,76 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import List from './pages/List';
 
 
+function Nav(props){
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    const element = props.topics[i];
+    lis.push(<li key={element.id}>
+      <a id={element.id} href={'/read/'+element.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{element.title}</a>
+      </li>)
+    
+  }
+  return(
+    <ol>{lis}</ol>
+  )
+}
+
+function Article(props){
+  return(
+    <article>
+        <h2>{props.title}</h2>
+        {props.body}
+      </article>
+  )
+}
+function Header(props){
+
+  return(
+    <header>
+      <h1><a href='/' onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+      </header>
+  )
+}
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const topics = [
+    {id:1,title:'html',body:'html is ...'},
+    {id:2,title:'css',body:'css is ...'},
+    {id:3,title:'js',body:'js is ...'}
+  ]
+  let content =null;
+  if(mode ==='WELCOME'){
+    content = <Article title='Welcome' body='Hello, WEB'/>
+  }else if(mode === 'READ'){
+    let title,body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}/>
+  }
+
   return (
     <div className="App">
-      <Container fluid>
-        <ButtonGroup>
-          <ButtonGroup>
-            <Button variant="primary" onClick={() => console.log("Primary")}>
-              Primary
-            </Button>
-            <Button variant="danger" onClick={() => console.log("Danger")}>
-              Danger
-            </Button>
-            <Button variant="dark" onClick={() => console.log("Dark")}>
-              Dark
-            </Button>
-          </ButtonGroup>
-        </ButtonGroup>
-      </Container>
+
+      <Header title='REACT' onChangeMode={()=>{
+        setMode('WELCOME');
+      }}/>
+
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+      }}/>
+      {content}
     </div>
       
       /* <nav>
